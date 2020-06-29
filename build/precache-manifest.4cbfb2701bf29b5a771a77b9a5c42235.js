c95464eafbf01515bf51c95e5956b94912129e08
Ярослав Коберник-Березовский <Yaroslavisgood@gmail.com>
2020-06-29 16:11:01 +0300

    Added build.

---
+++ build/precache-manifest.4cbfb2701bf29b5a771a77b9a5c42235.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d03b1ea80b63381e593919f9e1984236",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5a5802822be5e07ff7e",
+    "url": "/static/css/main.ba31ed85.chunk.css"
+  },
+  {
+    "revision": "bc9a5835d55d8620e55d",
+    "url": "/static/js/2.463823b0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.463823b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5a5802822be5e07ff7e",
+    "url": "/static/js/main.9f739d09.chunk.js"
+  },
+  {
+    "revision": "c7d969fe32d377bf81ff",
+    "url": "/static/js/runtime-main.b33ce693.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
